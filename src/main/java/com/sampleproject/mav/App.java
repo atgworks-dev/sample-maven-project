e2f206bdef7e9785800a19782d4a4d27523f8896
atgworks <atgworks@atgworks.com>
2018-09-20 13:57:39 -0400

    changes to App java file

--- src/main/java/com/sampleproject/mav/App.java
+++ src/main/java/com/sampleproject/mav/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! This is Maven running from Jenkins" );
+        System.out.println( "Hello World!" );
     }
 }
